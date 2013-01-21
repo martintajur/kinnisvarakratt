3855b2bce4dade14cfbb8235a526f68c605592ad
Martin Tajur <martin@tajur.ee>
2013-01-21 20:03:38 +0200

    majaosad otsingusse juurde

--- index.js
+++ index.js
@@ -178,19 +178,25 @@ db.query('SELECT * from objects LIMIT 1', function(err, rows, fields) {
 			});
 		};
 
-		// KV.ee Uued majad Harjumaal
-		kvSearch("maja", "http://www.kv.ee/?act=search.simple&deal_type=3&county=1&parish=0&energy_cert_val=0&price_min=&price_max=" + config.search.maja.maxHind + "&price_type=1&keyword=&search=Otsi&recent=1&orderby=cdwl");
+		// KV.ee majad Harjumaal
+		kvSearch('maja', 'http://www.kv.ee/?act=search.simple&deal_type=3&county=1&parish=0&energy_cert_val=0&price_min=&price_max=' + config.search.maja.maxHind + '&price_type=1&keyword=&search=Otsi&recent=1&orderby=cdwl');
 		
-		// KV.ee Uued korterid Tallinnas
-		kvSearch("korter", "http://www.kv.ee/?act=search.simple&deal_type=1&county=1&parish=421&county=1&parish=0&energy_cert_val=0&price_min=&price_max=" + config.search.maja.maxHind + "&price_type=1&keyword=&search=Otsi&recent=1&orderby=cdwl&rooms_min=" + config.search.korter.minTube + "&rooms_max=&area_min=" + config.search.korter.minSuurus);
+		// KV.ee korterid Tallinnas
+		kvSearch('korter', 'http://www.kv.ee/?act=search.simple&deal_type=1&county=1&parish=421&county=1&parish=0&energy_cert_val=0&price_min=&price_max=' + config.search.maja.maxHind + '&price_type=1&keyword=&search=Otsi&recent=1&orderby=cdwl&rooms_min=' + config.search.korter.minTube + '&rooms_max=&area_min=' + config.search.korter.minSuurus);
 
-		// EkspressKinnisvara Uued korterid Tallinnas
-		ekspressKinnisvaraSearch("korter", "http://www.ekspresskinnisvara.ee/est/otsing/?ot=1&obj=0&t=1&m=1&lv=1&la=0&yp_a=" + config.search.korter.minSuurus + "&yp_k=&ks_a=&ks_k=&l1=2&h_a=&h_k=" + config.search.maja.maxHind + "&l2=2&h2_a=&h2_k=&ea_a=&ea_k=&ta_a=" + config.search.korter.minTube + "&ta_k=&kv_a=&kv_k=&om=0&sk=0&my=0&m6=0&mt=0&lv2=0&ky=0&sort=U&Vk=1&q=&otsi_bt.x=46&otsi_bt.y=8&fid=&mid=&__acform__reqid=");
+		// KV.ee majaosad Tallinnas
+		kvSearch('majaosa', 'http://www.kv.ee/?act=search.simple&company_id=&broker_id=&recent=0&coords=&price_m2_min=0&price_m2_max=0&bid_objects=&years_default=20&deposite_in_percents_default=30&intress_default=3.5&agent=0age_size=100&deal_type=11&county=1&parish=0&energy_cert_val=0&price_min=&price_max=' + config.search.maja.maxHind + '&price_type=1&keyword=&floors_min=&floors_max=&area_total_min=' + config.search.korter.minSuurus + '&area_total_max=&area_ground_min=&area_ground_max=&search=Otsi&orderby=cdwl&recent=1');
 
-		// EkspressKinnisvara Uued korterid Tallinnas
-		ekspressKinnisvaraSearch("maja", "http://www.ekspresskinnisvara.ee/est/otsing/?ot=2&obj=0&t=1&m=1&lv=0&la=0&yp_a=%27&yp_k=&ks_a=&ks_k=&l1=2&h_a=&h_k=" + config.search.maja.maxHind + "&l2=2&h2_a=&h2_k=&ea_a=&ea_k=&ta_a=%27&ta_k=&kv_a=&kv_k=&om=0&sk=0&my=0&m6=0&mt=0&lv2=0&ky=0&sort=U&q=&otsi_bt.x=38&otsi_bt.y=12&fid=&mid=&__acform__reqid=");
+		// EkspressKinnisvara korterid Tallinnas
+		ekspressKinnisvaraSearch('korter', 'http://www.ekspresskinnisvara.ee/est/otsing/?ot=1&obj=0&t=1&m=1&lv=1&la=0&yp_a=' + config.search.korter.minSuurus + '&yp_k=&ks_a=&ks_k=&l1=2&h_a=&h_k=' + config.search.maja.maxHind + '&l2=2&h2_a=&h2_k=&ea_a=&ea_k=&ta_a=' + config.search.korter.minTube + '&ta_k=&kv_a=&kv_k=&om=0&sk=0&my=0&m6=0&mt=0&lv2=0&ky=0&sort=U&Vk=1&q=&otsi_bt.x=46&otsi_bt.y=8&fid=&mid=&__acform__reqid=');
 
-		// City24.ee Uued majad Harjumaal
+		// EkspressKinnisvara majad Tallinnas
+		ekspressKinnisvaraSearch('maja', 'http://www.ekspresskinnisvara.ee/est/otsing/?ot=2&obj=0&t=1&m=1&lv=0&la=0&yp_a=%27&yp_k=&ks_a=&ks_k=&l1=2&h_a=&h_k=' + config.search.maja.maxHind + '&l2=2&h2_a=&h2_k=&ea_a=&ea_k=&ta_a=%27&ta_k=&kv_a=&kv_k=&om=0&sk=0&my=0&m6=0&mt=0&lv2=0&ky=0&sort=U&q=&otsi_bt.x=38&otsi_bt.y=12&fid=&mid=&__acform__reqid=');
+
+		// EkspressKinnisvara majaosad Tallinnas
+		ekspressKinnisvaraSearch('majaosa', 'http://www.ekspresskinnisvara.ee/est/otsing/?ot=7&obj=0&t=1&m=1&lv=0&la=0&yp_a=' + config.search.korter.minSuurus + '&yp_k=&ks_a=&ks_k=&l1=2&h_a=&h_k=' + config.search.maja.maxHind + '&l2=2&h2_a=&h2_k=&ea_a=&ea_k=&ta_a=&ta_k=&kv_a=&kv_k=&om=0&sk=0&my=0&m6=0&mt=0&lv2=0&ky=0&sort=U&sa=1&q=&otsi_bt.x=39&otsi_bt.y=17&fid=&mid=&__acform__reqid=');
+
+		// City24.ee majad Harjumaal
 		city24Search('maja', {
 			pageId: '4',
 			objId: 'SearchObject',
@@ -204,7 +210,22 @@ db.query('SELECT * from objects LIMIT 1', function(err, rows, fields) {
 			search_size: 100
 		});
 
-		// City24.ee Uued korterid Tallinnas
+		// City24.ee majaosad Harjumaal
+		city24Search('majaosa', {
+			pageId: '4',
+			objId: 'SearchObject',
+			stateId: 'ShowResults',
+			eventId: '',
+			search_oldest: moment().subtract('days', 1).format("DD.MM.YYYY"),
+			search_reo_type: 'HOUSE_PART',
+			search_trans: 'TRANSACTION_SALE',
+			search_county: 'COUNTY_HARJUMAA',
+			search_area1: config.search.korter.minSuurus,
+			search_price2: config.search.maja.maxHind,
+			search_size: 100
+		});
+
+		// City24.ee korterid Tallinnas
 		city24Search('korter', {
 			pageId: '4',
 			objId: 'SearchObject',
